29396a43328cd5d47f6b21da18d1e5ee686f580d
BartoszLauks <bartosz.lauksstu@interia.pl>
2024-06-04 11:47:37 +0200

    Add packages orm, profiler, security, twig

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir ./config/jwt
 COMPOSER_ALLOW_SUPERUSER=1 docker compose exec -T vehicle-rental-management-system-php composer self-update
 COMPOSER_ALLOW_SUPERUSER=1 docker compose exec -T vehicle-rental-management-system-php composer update --no-interaction --classmap-authoritative --optimize-autoloader
 
-#docker compose exec -T vehicle-rental-management-system-php php bin/console doctrine:database:create --if-not-exists
+docker compose exec -T vehicle-rental-management-system-php php bin/console doctrine:database:create --if-not-exists
 #docker compose exec -T vehicle-rental-management-system-php php bin/console doctrine:migrations:migrate --no-interaction
 
 #JWT
